b9122300bf1ec28e05dc9b3137c1c9acfd4737f5
janedao-log <phuongtrang.dao@ucalgary.ca>
2026-01-28 09:56:20 -0700

    Update app.py

--- app.py
+++ app.py
@@ -6,12 +6,16 @@ if "tasks" not in st.session_state:
   st.session_state.tasks = []
 
 #Input information
-new_task = st.text_input("New Task")
+# new_task = st.text_input("New Task")
+Year = st.text_input("Năm Kinh Nghiệm")
+salary = st.number_input("Mức Lương", min_value=0, value=o)
 
 #Processing
-if st.button("Add Task"):
-  st.session_state.tasks.append(new_task)
+if st.button("Add Info"):
+  # st.session_state.tasks.append(new_task)
+  real_salary = salary*1.2 if int(Year) > 5 else salary
 
 #Show output
-for i, task in enumerate(st.session_state.tasks):
-  st.write(f"ID - {i}: Value - {task}")
+# for i, task in enumerate(st.session_state.tasks):
+#   st.write(f"ID - {i}: Value - {task}")
+st.write(f"Real Salary: {real_salary}")
